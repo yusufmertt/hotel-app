555b8b49630efda98939ffa41627ca1f98ea9e4e
Yusuf Mert Çakmak <ymert60@hotmail.com>
2019-09-12 04:31:10 +0300

    favicon

---
+++ precache-manifest.8bad1ad72a47012237d3304eae167d40.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f887187f074914e69028fe070c068a5d",
+    "url": "/hotel-app/index.html"
+  },
+  {
+    "revision": "d55f9a39aeda4b2e89cc",
+    "url": "/hotel-app/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "62ab8df4a8dd61fa9e22",
+    "url": "/hotel-app/static/css/main.e9333e41.chunk.css"
+  },
+  {
+    "revision": "d55f9a39aeda4b2e89cc",
+    "url": "/hotel-app/static/js/2.b3643ade.chunk.js"
+  },
+  {
+    "revision": "62ab8df4a8dd61fa9e22",
+    "url": "/hotel-app/static/js/main.e022f28a.chunk.js"
+  },
+  {
+    "revision": "db62089e31c0f2527b87",
+    "url": "/hotel-app/static/js/runtime~main.cba0f22e.js"
+  },
+  {
+    "revision": "eaba4394b401a04ffc549f53bbb253a0",
+    "url": "/hotel-app/static/media/family-d.eaba4394.jpeg"
+  },
+  {
+    "revision": "403776ba810ad7d946ab270c1c86b755",
+    "url": "/hotel-app/static/media/family.403776ba.jpg"
+  },
+  {
+    "revision": "f02d6a6c4801e4b4daba560e8707071a",
+    "url": "/hotel-app/static/media/logo.f02d6a6c.png"
+  },
+  {
+    "revision": "d9ba887a9c3af8f82062b82e901d4323",
+    "url": "/hotel-app/static/media/resort-2.d9ba887a.jpg"
+  },
+  {
+    "revision": "9609e41f091ee8b7278e848ae8ff7894",
+    "url": "/hotel-app/static/media/resort.9609e41f.jpg"
+  },
+  {
+    "revision": "baba703d4d681d9468b4e3069a05f6de",
+    "url": "/hotel-app/static/media/single-d.baba703d.jpeg"
+  },
+  {
+    "revision": "b033bf686b791250f0e29c69f6919fa7",
+    "url": "/hotel-app/static/media/single.b033bf68.jpg"
+  },
+  {
+    "revision": "72ed7491fd3dc3ae51ad0a5b2fba4671",
+    "url": "/hotel-app/static/media/suite-d.72ed7491.jpg"
+  },
+  {
+    "revision": "a1ddd895b0fb05f50335775d05293b09",
+    "url": "/hotel-app/static/media/suite.a1ddd895.jpg"
+  }
+]);
